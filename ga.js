748f193504551a2fe74bcda55cd962dc7c61c0bc
Mishig Davaadorj <dmishig@gmail.com>
2018-12-06 23:13:05 -0700

    feat: initial run

--- ga.js
+++ ga.js
@@ -23,13 +23,99 @@ class Generation {
      * @param {object}
      */
 
-    initialize(Ragdoll, sceneSize) {
+    initialize(Robot, sceneSize) {
         const y = (this.world.vtcl.max + this.world.vtcl.min) / 2;
         const hztl = this.world.hztl;
         for (let i = 0; i < this.population; i++) {
             const x = Math.round(Math.random() * (hztl.max - hztl.min) + hztl.min);
-            let robot = new Ragdoll(this.world, sceneSize, x, y);
+            let robot = new Robot(this.world, sceneSize, x, y, i);
             this.species.push(robot);
         }
+    }
+
+    test(){
+        this.species.forEach((robot) => {
+            robot.start();
+        });
+    }
+
+    evolve(){
+        // Store High Score
+        this.generation += 1;
+        let gen_highscore = Math.max.apply(Math, this.species.map(o => o.score));
+        this.high_score = gen_highscore > this.high_score ? gen_highscore : this.high_score;
+        
+        // Calculate Total Score of this Generation
+        let total_score = 0;
+        this.species.forEach((creature) => { total_score += creature.score });
+
+        // Assign Fitness to each creature
+        this.progress = (total_score / this.population) - this.avg_score
+        this.avg_score = total_score / this.population;
+        for (let i = 0; i < this.population; i++) {
+            this.species[i].fitness = this.species[i].score / total_score;
+        };
+
+        // Store new generation temporarily in this array
+        let new_generation = [];
+
+        // Breeding
+        let total_fitness = 0;
+        let score_x = new Array(this.population);
+        for (let j = 0; j < this.population; j++) {
+            score_x[j] = new Array(2);
+            score_x[j][0] = j;
+            total_fitness += this.species[j].fitness;
+            score_x[j][1] = total_fitness;
+        }
+        for (let i = 0; i < this.population; i++) {
+
+            let r1 = Math.random();
+            let r2 = Math.random();
+
+            // console.log("r" , r);
+            let parentA_id = 0;
+            let parentB_id = 0;
+            for (let j = 0; j < this.population; j++) {
+
+                if (score_x[j][1] >= r1) {
+                    parentA_id = score_x[j][0];
+                    // console.log("selected", score_x[j][1]);
+                    break;
+                }
+            }
+
+            for (let j = 0; j < this.population; j++) {
+
+                if (score_x[j][1] >= r2) {
+                    parentB_id = score_x[j][0];
+                    break;
+                }
+            }
+            // console.log("best", score_x[0][0]);
+            // let parentA = this.species[score_x[0][0]].clone();
+            // let parentB = this.species[score_x[1][0]].clone();
+
+            let parentA = this.species[parentA_id].clone();
+            let parentB = this.species[parentB_id].clone();
+
+            let child = parentA.crossover(parentB);
+            child.mutate();
+            child.id = i;
+            child.parents = [{ id: parentA.id, score: this.species[parentA.id].score }, { id: parentB.id, score: this.species[parentB.id].score }];
+            new_generation.push(child);
+        }
+
+        // Kill Current Generation.
+        // i.e. Remove their bodies from MatterJS World and dispose their brain
+        for (let i = 0; i < this.population; i++) {
+            this.species[i].kill();
+        }
+
+        // Add new children to the current generation
+        this.species = new_generation;
+        // for (let i = 0; i < this.population; i++) {
+        //     this.species[i].add_to_world(this.world);
+        // }
     }
 }
--- robot.js
+++ robot.js
@@ -1,8 +1,15 @@
 const pl = planck, Vec2 = pl.Vec2;
-class Ragdoll{
-    constructor(world,size,x,y){
+class Robot{
+    constructor(world,size,x,y,id){
         this.init = false;
         this.world = world;
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        this.id = id;
+        this.score = 0;
+        this.fitness = 0;
+        this.parents = [];
         this.bodyParts = {};
         this.joints = {};
         this.createBody(size,x,y);
@@ -10,7 +17,7 @@ class Ragdoll{
         this.bodyPartsKeys = Object.keys(this.bodyParts);
         this.brain = new NeuralNetwork(this.jointsKeys.length+1, 30, this.bodyPartsKeys.length);
         this.init = true;
-    }
+    };
     createBody(size,x,y){
         this.coreBody(size, x, y);
         this.limb('leftLeg',size / 3, x-size / 1.5, y-size / 0.285);
@@ -21,12 +28,13 @@ class Ragdoll{
         this.createJoint('rightLegUp', this.bodyParts.lower, this.bodyParts.rightLegUp, pl.Vec2(x+size/1.3333,y-size/2), Math.PI / 8, Math.PI / 2);
         this.createJoint('leftArmUp', this.bodyParts.upper, this.bodyParts.leftArmUp, {x:x-size/1.3333,y:y+size/0.4}, Math.PI / 3);
         this.createJoint('rightArmUp', this.bodyParts.upper, this.bodyParts.rightArmUp, { x: x+size / 1.3333, y: y+size / 0.4 }, Math.PI / 3);
-    }
-    rot(){
-    //     const hd = this.bodyParts.head.c_position.c.y;
-    //    const x = hd.c_position.c.x;
-    //    const y = hd.c_position.c.y;
-    }
+    };
+    update(){
+        // should be called every frame
+        const vtcl = this.world.vtcl;
+        const head_y = this.mapRange(this.bodyParts.head.c_position.c.y, vtcl.min, vtcl.max);
+        this.score = head_y * 100;
+    };
     coreBody(size, x, y) {
         const w = size, h = size;
         const boxshpUp = pl.Box(w, h);
@@ -38,7 +46,7 @@ class Ragdoll{
         this.bodyParts.upper = this.body_fixture(x, y + h * 1.5, boxshpUp);
         this.createJoint('spine', this.bodyParts.upper, this.bodyParts.lower, { x: x, y: y + h / 2 }, Math.PI / 8);
         this.createJoint('neck', this.bodyParts.upper, this.bodyParts.head, { x: x, y: y + h * 3 }, Math.PI / 6);
-    }
+    };
     limb(name,size, x, y, rotate=false, left=true) {
         var w = size, h = 3 * size;
         if (rotate) w = 3 * size, h = size;
@@ -63,13 +71,13 @@ class Ragdoll{
         this.bodyParts[name + 'Up'] = upper;
         this.bodyParts[name + 'Low'] = lower;
         return upper;
-    }
+    };
     body_fixture(x, y, shp, density = 1.0) {
         const body_part = this.world.createDynamicBody(Vec2(x, y));
         body_part.createFixture(shp, density);
         body_part.m_fixtureList.m_filterGroupIndex = -1;
         return body_part;
-    }
+    };
     createJoint(name, bodyA, bodyB, anchor, lowerAngle, upperAngle = lowerAngle){
         const limits = {
             lowerAngle: -lowerAngle,
@@ -78,10 +86,10 @@ class Ragdoll{
         }
         const joint = this.world.createJoint(pl.RevoluteJoint(limits, bodyA, bodyB, anchor));
         this.joints[name] = joint;
-    }
+    };
     mapRange(num, in_min, in_max, out_min=0.0, out_max=1.0){
         return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    }
+    };
     createBrainInput(){
         let input = [];
         this.jointsKeys.forEach((jointKey) => {
@@ -94,7 +102,7 @@ class Ragdoll{
         const head_y = this.mapRange(this.bodyParts.head.c_position.c.y, vtcl.min, vtcl.max);
         input.push(head_y);
         return input;
-    }
+    };
     think(){
         let input = this.createBrainInput();
         let result = this.brain.predict(input);
@@ -104,10 +112,100 @@ class Ragdoll{
             const bodyPart = this.bodyPartsKeys[i];
             this.bodyParts[bodyPart].applyAngularImpulse(impulse);
         }
-    }
+    };
     start() {
         setInterval(() => {
-            this.think()
+            this.think();
         }, 100)
-    }
-}
+    };
+    clone() {
+        let new_robot = new Robot(this.world, this.size, this.x, this.y, this.id);
+        new_robot.brain.dispose();
+        new_robot.brain = this.brain.clone();
+        return new_robot;
+    };
+    kill(){
+        // remove from world
+        // this.world.destroyBody();
+        Object.values(this.bodyParts).forEach((bodyPart) => {
+            this.world.destroyBody(bodyPart);
+        });
+        Object.values(this.joints).forEach((jointsPart) => {
+            this.world.destroyBody(jointsPart);
+        });
+    };
+    mutate() {
+        const self = this;
+        function fn(x) {
+            if (self.random(1) < 0.05) {
+                let offset = self.randomGaussian() * 0.5;
+                let newx = x + offset;
+                return newx;
+            }
+            return x;
+        }
+
+        let ih = this.brain.input_weights.dataSync().map(fn);
+        let ih_shape = this.brain.input_weights.shape;
+        this.brain.input_weights.dispose();
+        this.brain.input_weights = tf.tensor(ih, ih_shape);
+
+        let ho = this.brain.output_weights.dataSync().map(fn);
+        let ho_shape = this.brain.output_weights.shape;
+        this.brain.output_weights.dispose();
+        this.brain.output_weights = tf.tensor(ho, ho_shape);
+    };
+    crossover(partner) {
+        let parentA_in_dna = this.brain.input_weights.dataSync();
+        let parentA_out_dna = this.brain.output_weights.dataSync();
+        let parentB_in_dna = partner.brain.input_weights.dataSync();
+        let parentB_out_dna = partner.brain.output_weights.dataSync();
+
+        let mid = this.score / partner.score;
+
+        //let mid = Math.floor(Math.random() * parentA_in_dna.length);
+        let child_in_dna = [...parentA_in_dna.slice(0, mid), ...parentB_in_dna.slice(mid, parentB_in_dna.length)];
+        let child_out_dna = [...parentA_out_dna.slice(0, mid), ...parentB_out_dna.slice(mid, parentB_out_dna.length)];
+
+        let child = this.clone();
+        let input_shape = this.brain.input_weights.shape;
+        let output_shape = this.brain.output_weights.shape;
+
+        child.brain.dispose();
+
+        child.brain.input_weights = tf.tensor(child_in_dna, input_shape);
+        child.brain.output_weights = tf.tensor(child_out_dna, output_shape);
+
+        return child;
+    };
+    random(min, max) {
+        var rand = Math.random();
+        if (typeof min === 'undefined') {
+            return rand;
+        } else if (typeof max === 'undefined') {
+            if (min instanceof Array) {
+                return min[Math.floor(rand * min.length)];
+            } else {
+                return rand * min;
+            }
+        } else {
+            if (min > max) {
+                var tmp = min;
+                min = max;
+                max = tmp;
+            }
+
+            return rand * (max - min) + min;
+        }
+    };
+    randomGaussian() {
+        var x1, x2, rad, y1;
+        do {
+            x1 = 2 * this.random() - 1;
+            x2 = 2 * this.random() - 1;
+            rad = x1 * x1 + x2 * x2;
+        } while (rad >= 1 || rad == 0);
+        var c = Math.sqrt(-2 * Math.log(rad) / rad);
+        return x1 * c;
+    };
+};
